60347f581fc2413507dc0a35fb0bfc066d5fdc85
Patrycja Grabiec <patr.grabiec@gmail.com>
2023-09-12 16:35:25 +0200

    Add products model structure

--- backend/app_1/models.py
+++ backend/app_1/models.py
@@ -5,3 +5,18 @@ class Nutrition(models.Model):
     
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class ProductNutrition(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    nutrition = models.ForeignKey(Nutrition, on_delete=models.CASCADE)
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"{self.product} - {self.nutrition}"
+    
