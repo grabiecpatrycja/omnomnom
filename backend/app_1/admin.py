4bf4c0375c30d97e1541a337dfb9908277df912d
Patrycja Grabiec <patr.grabiec@gmail.com>
2023-09-13 20:24:26 +0200

    Add products model structure

--- backend/app_1/admin.py
+++ backend/app_1/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from app_1.models import Nutrition
+from app_1.models import Nutrition, Product, ProductNutrition
 
 admin.site.register(Nutrition)
+admin.site.register(Product)
+admin.site.register(ProductNutrition)
