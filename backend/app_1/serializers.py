987d0c1668dfe33bb020a13b931295be24a0ae68
Patrycja Grabiec <patr.grabiec@gmail.com>
2023-09-15 15:17:59 +0200

    Add product endopoints

--- backend/app_1/serializers.py
+++ backend/app_1/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from app_1.models import Nutrition
+from app_1.models import Nutrition, Product
 
 class NutritionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nutrition
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
--- backend/app_1/views.py
+++ backend/app_1/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from app_1.models import Nutrition
-from app_1.serializers import NutritionSerializer
+from app_1.models import Nutrition, Product
+from app_1.serializers import NutritionSerializer, ProductSerializer
 
 class NutritionViewSet(viewsets.ModelViewSet):
     queryset = Nutrition.objects.all()
     serializer_class = NutritionSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
