d197077834b45ca5d6c737e94a4aca9ba9baa362
grabiecpatrycja <117544065+grabiecpatrycja@users.noreply.github.com>
2023-09-26 15:25:08 +0200

    Product nutrition2 (#6)
    * Add endpoint to mapping bewteen product and nutritions
    
    * Add endpoint to mapping between product and nutrintions 2.0
    
    * Add endpoint to mapping between product and nutrintions 3.0
    
    * Add transaction
    
    * Add test transaction

--- backend/app_1/tests.py
+++ backend/app_1/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from app_1.models import Nutrition, Product
+from app_1.models import Nutrition, Product, ProductNutrition
 
 class NutritionTestCase(TestCase):
     def setUp(self):
@@ -90,3 +90,38 @@ class ProductTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(Product.DoesNotExist):
             object.refresh_from_db()
+
+class ProductNutritionTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.product = Product.objects.create(name='test_product')
+        self.nutrition_1 = Nutrition.objects.create(name='test_nutrition_1')
+        self.nutrition_2 = Nutrition.objects.create(name='test_nutrition_2')
+
+    def test_create_object(self):
+        data = [{'product':self.product.id, 'nutrition': self.nutrition_1.id, 'value': 120},{'product':self.product.id, 'nutrition': self.nutrition_2.id, 'value': 40}]
+        url = reverse('products-nutritions', args=[self.product.id])
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ProductNutrition.objects.count(), 2)
+
+    def test_create_object_error(self):
+        data = [{'product':self.product.id, 'nutrition': self.nutrition_1.id, 'value': 120},{'product':self.product.id, 'nutrition': self.nutrition_2.id, 'value': "króliczek"}]
+        url = reverse('products-nutritions', args=[self.product.id])
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(ProductNutrition.objects.count(), 0)
+
+    def test_update_object(self):
+        object_1 = ProductNutrition.objects.create(product=self.product, nutrition=self.nutrition_1, value=120)
+        object_2 = ProductNutrition.objects.create(product=self.product, nutrition=self.nutrition_2, value=20)
+        updated_data = [{'nutrition': self.nutrition_1.id, 'value': 120}, {'nutrition': self.nutrition_2.id, 'value': 40}]
+        url = reverse('products-nutritions', args=[self.product.id])
+        response = self.client.put(url, updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        object_1.refresh_from_db()
+        self.assertEqual(object_1.value, 120)
+        object_2.refresh_from_db()
+        self.assertEqual(object_2.value, 40)
+        self.assertEqual(ProductNutrition.objects.count(), 2)
+
--- backend/app_1/views.py
+++ backend/app_1/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets
-from app_1.models import Nutrition, Product
-from app_1.serializers import NutritionSerializer, ProductSerializer
+from django.db import transaction
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from app_1.models import Nutrition, Product, ProductNutrition
+from app_1.serializers import NutritionSerializer, ProductSerializer, ProductNutritionSerializer
 
 class NutritionViewSet(viewsets.ModelViewSet):
     queryset = Nutrition.objects.all()
@@ -9,3 +12,20 @@ class NutritionViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['PUT'], serializer_class=ProductNutritionSerializer)
+    @transaction.atomic
+    def nutritions(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        data = request.data
+        for d in data:
+            nutrition = d.get('nutrition')
+            try:
+                product_nutrition = ProductNutrition.objects.get(product=product, nutrition=nutrition)
+                serializer = ProductNutritionSerializer(product_nutrition, data=d)
+            except ProductNutrition.DoesNotExist:
+                serializer = ProductNutritionSerializer(data=d)
+
+            serializer.is_valid(raise_exception=True)
+            serializer.save(product=product)
+        return Response(status=status.HTTP_201_CREATED)
