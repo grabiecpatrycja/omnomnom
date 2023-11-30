f8b6729b726d8d8ca62040d5f96cd86b3d7029d9
Jerzy Spendel <jspendel@gmail.com>
2023-11-30 22:33:37 +0100

    Make container's weight history work

--- frontend/lib/container/history/widgets.dart
+++ frontend/lib/container/history/widgets.dart
@@ -2,8 +2,31 @@ import 'package:calcounter/container/history/history_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'history_entry.dart';
+
 
 class ContainerHistory extends StatelessWidget {
+  Widget onSuccess(HistoryState state, context) {
+        return Table(
+            defaultColumnWidth: const FixedColumnWidth(20),
+            children: state.entries.map((HistoryEntryModel entry) {
+              return TableRow(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      width: 50,
+                      child: Text("${entry.date.year}-${entry.date.month}-${entry.date.day}, ${entry.date.hour}:${entry.date.minute}"),
+                    ),
+                    Container(
+                        width: 50,
+                        child: Text(entry.mass.toString())
+                    )
+                  ]
+              );
+            }).cast<TableRow>().toList()
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HistoryBloc, HistoryState>(builder: (context, state) {
@@ -11,7 +34,7 @@ class ContainerHistory extends StatelessWidget {
         case HistoryStatus.initial:
           return const Text("Loading...");
         case HistoryStatus.success:
-          return const Text("Załadowano :o");
+          return onSuccess(state, context);
         case HistoryStatus.failure:
           return const Text("wyjebawszy coś");
       }
